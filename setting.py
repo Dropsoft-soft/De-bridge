267b3a778e1fb68ab725ee8f19dbaba307ff549a
4taras4 <4taras4@gmail.com>
2024-04-22 20:38:19 +0300

    Update setting.py

--- setting.py
+++ setting.py
@@ -6,6 +6,3 @@ CHECK_GWEI      = False         # True / False. Use gas limit or no
 MAX_GWEI        = 190           # gas in gwei (https://etherscan.io/gastracker)
 RANDOM_WALLET   = False         # Randomize wallets
 MAX_THREADS     = 1             # How much do wallets in paralel time at the same time
-
-CHECK_GWEI      = False         # True / False. Use gas limit or no
-MAX_GWEI        = 190           # gas in gwei (https://etherscan.io/gastracker)
